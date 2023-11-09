32132e0d543d1823864084d304e94b2fcc091944
John Fletcher <john@bunbury28.plus.com>
2023-11-09 15:27:05 +0000

    Create CANSAM3X8E.h
    Start header file

---
+++ VLCB_DUE3EEPROM/CANSAM3X8E.h
@@ -0,0 +1,4 @@
+// Copyright (C) Sven Rosvall (sven@rosvall.ie) (C( John Fletcher (john@bunbury28.plus.com)
+// This file is part of VLCB-Arduino project on https://github.com/SvenRosvall/VLCB-Arduino
+// Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+// The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0/
